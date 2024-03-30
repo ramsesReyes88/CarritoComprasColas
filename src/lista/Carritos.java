b700d4bd58f86751a9e33fbb70b1931b97cc889c
ramsesreyes8 <0322103800@ut-tijuana.edu.mx>
2024-03-29 21:10:52 -0700

    DeclararArreglo

---
+++ src/lista/Carritos.java
@@ -0,0 +1,22 @@
+package lista;
+
+public class Carritos {
+    private String codigo;
+    private String tipo;
+
+    public void setCodigo(String codigo){
+        this.codigo = codigo;
+    }
+
+    public String getCodigo(){
+        return codigo;
+    }
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+
+    public String getTipo(){
+        return tipo;
+    }
+}
--- src/lista/Pila.java
+++ src/lista/Pila.java
@@ -1,5 +1,36 @@
 package lista;
 
 public class Pila {
-    
+    int max = 20;
+    int cima = 0;
+    Carritos[] carrito = new Carritos[max];
+
+    public Pila() {
+        for (int i = 0; i < max; i++) {
+            carrito[i] = new Carritos();
+        }
+    }
+
+    /* Cantidad Maxima de Elementos que se pueden agregar */
+
+    public int maxElementos() {
+        return max;
+    }
+
+    /* Cantidad de elementos agregados */
+
+    public int numElementos() {
+        return cima;
+    }
+
+    /* Saber si la pila esta llena */
+
+    public boolean pilaLlena() {
+        return cima >= max;
+    }
+
+    public boolean pilaVacia() {
+        return cima == 0;
+    }
+
 }
