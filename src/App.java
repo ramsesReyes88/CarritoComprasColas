a346f62c0e0aa9c5c19ece238d77f69cde322aba
ramsesreyes8 <0322103800@ut-tijuana.edu.mx>
2024-03-29 22:10:50 -0700

    FuncionesAgregadas

--- src/App.java
+++ src/App.java
@@ -1,5 +1,79 @@
+import java.util.Scanner;
+
+import lista.Pila;
+
 public class App {
+    static Scanner read = new Scanner(System.in);
+    static Pila filaCarrito = new Pila();
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       int opcion;
+       do {
+        opcion = menu();
+        switch (opcion) {
+            case 1:
+                agregar();
+                break;
+            case 2:
+                retirar();
+                break;
+                
+            case 3:
+                imprimir();
+                break;
+        }
+    } while (opcion != 4);
+    }
+
+    public static int menu(){
+        int opcion;
+        do {
+            clear();
+            System.out.println("Gestion de Carritos de Compra");
+            System.out.println("1.Ingresar un Carrito");
+            System.out.println("2.Retirar Carrito");
+            System.out.println("3.Carritos en total");
+            System.out.println("4.Salir del Programa");
+            System.out.println("Escoja su opcion: ");
+            opcion = read.nextInt();
+        } while (opcion < 1 || opcion > 4);
+        return opcion;
+    }
+
+    public static void agregar(){
+        String codigo,tipo;
+        clear();
+        System.out.println("agregue los datos ");
+        System.out.println("Codigo:       ");
+        codigo = read.next();
+        System.out.println("Tipo");
+        tipo = read.next();   
+        filaCarrito.ingresarCarrito(codigo, tipo);
+        enter(); 
+    }
+
+    public static void retirar(){
+        clear();
+        System.out.println("Retirar el ultimo Carrito");
+        filaCarrito.retirar();
+        enter();
+    }
+
+    public static void imprimir(){
+        clear();
+        System.out.println("Lista de Carritos");
+        filaCarrito.imprimir();
+        enter();
+    }
+
+    public static void enter(){
+        Scanner continuar = new Scanner(System.in);
+        continuar.useDelimiter("\n");
+        System.out.println("Presione enter para Continuar...");
+        continuar.next();
+    }
+
+    public static void clear() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
     }
 }
--- src/lista/Pila.java
+++ src/lista/Pila.java
@@ -29,8 +29,49 @@ public class Pila {
         return cima >= max;
     }
 
+    /*Saber si la pila esta vacia */
     public boolean pilaVacia() {
         return cima == 0;
     }
 
+    public void ingresarCarrito(String codigo, String tipo) {
+        if (pilaVacia()) {
+            carrito[cima].setCodigo(codigo);
+            carrito[cima].setTipo(tipo);
+            cima++;
+
+            if (pilaLlena()) {
+                System.out.println("¡La fila de carritos se ha llenado!");
+            }
+        } else {
+            System.out.println("No hay espacio disponible para agregar más carritos.");
+        }
+    }//agregar
+
+    /* Método para retirar un carrito */
+    public void retirar() {
+        if (pilaVacia()) {
+            System.out.println("No hay paquetes para retirar");
+        } else {
+            cima--;
+            System.out.println("Carrito retirado");
+            System.out.println("Codigo       " + carrito[cima].getCodigo());
+            System.out.println("Tipo:        " + carrito[cima].getTipo());
+            carrito[cima].setCodigo(null);
+            carrito[cima].setTipo(null);
+        }
+    }//retirar
+
+    /* Método para mostrar el total de carritos disponibles */
+    public void imprimir() {
+        if (pilaVacia()) {
+            System.out.println("Actualmente no hay carritos en ninguna fila");
+        } else {
+            System.out.println("Codigo \t Tipo");
+            for(int i = 0;i<cima;i++){
+                System.out.println(carrito[i].getCodigo());
+                System.out.println("\t" + carrito[i].getTipo());
+            }
+        }
+    }//imprimir
 }
